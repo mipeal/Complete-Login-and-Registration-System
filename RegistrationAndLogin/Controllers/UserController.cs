ab6b451ee3eb551e5572058a1c0c338577f0b9e8
Mujahid Islam Peal <peal818@diu.edu.bd>
2017-06-07 17:59:28 +0600

    Login done

--- RegistrationAndLogin/Controllers/UserController.cs
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using RegistrationAndLogin.Models;
 using System.Net.Mail;
 using System.Web.Services.Description;
 using System.Net;
+using System.Web.Security;
 
 namespace RegistrationAndLogin.Controllers
 {
@@ -60,7 +61,7 @@ namespace RegistrationAndLogin.Controllers
                 #region //Send Email to user
                 SendVerificationLink(user.EmailID, user.ActivationCode.ToString());
                 message = @"Registration succesfully done. Account verification link has been sent to 
-                            your email id :'" + user.EmailID + "'";
+                            your email id -> " + user.EmailID ;
                 Status = true;
                 #endregion
 
@@ -74,12 +75,92 @@ namespace RegistrationAndLogin.Controllers
             return View(user);
         }
 
+        //Verify Account
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)
+        {
+            bool status = false;
+            using (UserInformationEntities db=new UserInformationEntities())
+            {
+                db.Configuration.ValidateOnSaveEnabled = false; //This line here I have added to avoid confirm password
+                                                                //does not match issue on save changes
+
+                var v = db.tblUsers.Where(a => a.ActivationCode == new Guid(id)).FirstOrDefault();
+                if (v != null)
+                {
+                    v.IsEmailVerified = true;
+                    db.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Request";
+                }
+            }
+            ViewBag.Status = status;
+            return View();
+        }
+
         //Login
-
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
         //Login post 
+        [HttpPost]
+        public ActionResult Login(UserLogin login, string ReturnUrl="")
+        {
+            string message = "";
 
+            ViewBag.Message = message;
+
+            using(UserInformationEntities db=new UserInformationEntities())
+            {
+                var v = db.tblUsers.Where(a => a.EmailID == login.EmailID).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(Crypto.Hash(login.Password),v.Password)==0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20;  // 525600min = 1 year
+                        var ticket = new FormsAuthenticationTicket(login.EmailID, login.RememberMe, timeout); ;
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+
+                        if (Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credentials provided";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credentials provided";
+                }
+            }
+
+            return View();
+        }
         //Logout
-
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
 
         //Verify Email
         [NonAction]
@@ -97,21 +178,21 @@ namespace RegistrationAndLogin.Controllers
         public void SendVerificationLink(string email, string code)
         {
             var verifyUrl = "/User/VerifyAccount/" + code;
-            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+            var link = Request.Url.AbsoluteUri.ToString() + verifyUrl;
 
-            var fromEmail = new MailAddress("mujahid.islam_peal@outlook.com", "SungWann Tours & Travel");
+            var fromEmail = new MailAddress("sungwanntoursntravels@gmail.com", "Sung Wann Tours & Travel");
             var toEmail = new MailAddress(email);
-            var fromEmailPassword = "rtt09876";
+            var fromEmailPassword = "SWTT0987";
 
             string subject = "Your account is succesfully created!";
 
             string body = @"<br/><br/>We're excited to tell you that your account for <strong>SungWann Tours & Travel</strong> is
-                            succesfully created. Please verify your account by clicking the link given below-><br/><br/>
-                            <a href='"+link+"'>"+link+"</a>";
+                            succesfully created. Please verify your account by <br/><br/>
+                            <a href='"+link+"'>clicking here!</a>";
 
             var smtp = new SmtpClient
             {
-                Host = "smtp-mail.outlook.com",
+                Host = "smtp.gmail.com",
             Port = 587,
             EnableSsl = true,
             DeliveryMethod=SmtpDeliveryMethod.Network,
